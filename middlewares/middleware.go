eccfa1e25810b3087d6140493545923367848524
Arun Dhyani <arun@clinikk.com>
2019-09-10 13:13:48 +0530

    bug fix in chainning middleware

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -13,16 +13,16 @@ func convertToHandler(handlerFunc http.HandlerFunc) http.Handler {
 // ChainHandlerFuncMiddlewares chains handlerFunc and middlewares
 func ChainHandlerFuncMiddlewares(handlerFunc http.HandlerFunc, middlewares ...mux.MiddlewareFunc) http.Handler {
 	handler := convertToHandler(handlerFunc)
-	for _, middleware := range middlewares {
-		handler = middleware(handler)
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		handler = middlewares[i](handler)
 	}
 	return handler
 }
 
 // ChainHandlerMiddlewares chains handlerFunc and middlewares
 func ChainHandlerMiddlewares(handler http.Handler, middlewares ...mux.MiddlewareFunc) http.Handler {
-	for _, middleware := range middlewares {
-		handler = middleware(handler)
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		handler = middlewares[i](handler)
 	}
 	return handler
 }
