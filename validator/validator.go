eaa877aefa59430b086a2e418c7b0e31b8eb22ce
Arun Dhyani <arun@clinikk.com>
2020-05-13 05:12:56 +0530

    validator with custom tags added

---
+++ validator/validator.go
@@ -0,0 +1,27 @@
+package validator
+
+import (
+	"regexp"
+
+	"gopkg.in/go-playground/validator.v9"
+)
+
+// validation  constants
+const (
+	IndiaPhoneNumberTag = "IndiaPhoneNumber"
+)
+
+// IsIndiaPhoneNumber checks parameter pattern for valid phone number of India
+func IsIndiaPhoneNumber(fl validator.FieldLevel) bool {
+	IndiaPhoneNumberRegexString := "^[0-9]{10}$"
+	IndiaPhoneNumberRegex := regexp.MustCompile(IndiaPhoneNumberRegexString)
+
+	return IndiaPhoneNumberRegex.MatchString(fl.Field().String())
+}
+
+// New return new instance of Validate
+func New() *validator.Validate {
+	validate := validator.New()
+	validate.RegisterValidation(IndiaPhoneNumberTag, IsIndiaPhoneNumber)
+	return validate
+}
