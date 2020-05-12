7c7ac58d689fc2c0b5d5c19b21b3667fd80ade3a
Arun Dhyani <arun@clinikk.com>
2020-05-13 02:04:31 +0530

    sql configuration refactored and bug fixes

--- datastore/sql/sql.go
+++ datastore/sql/sql.go
@@ -3,15 +3,18 @@ package sql
 import (
 	"database/sql"
 
+	// mysql driver
+	_ "github.com/go-sql-driver/mysql"
+
 	"github.com/opentracing/opentracing-go"
 )
 
 // Configuration provides configuration for SQL Driver
 type Configuration struct {
 	DSN                   string `required:"true"`
-	MaxIdleConnections    int    `required:"true"`
-	MaxOpenConnections    int    `required:"true"`
-	ConnectionMaxLifetime string `required:"true"`
+	MaxIdleConnections    int    `required:"true" split_words:"true"`
+	MaxOpenConnections    int    `required:"true" split_words:"true"`
+	ConnectionMaxLifetime string `required:"true" split_words:"true"`
 }
 
 // CreatePool creates connection pool for SQL server
